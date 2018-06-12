5144f39e006d8ddb5d72b024938cb28b6b0100d6
John Fish <john@johnafish.ca>
2018-06-11 21:12:23 -0400

    Prints message of number of comments fetched

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python
 
 # Usage example:
-# python comments.py --videoid='<video_id>' --text='<text>'
+# python comments.py --videoid='<video_id>'
 
 import httplib2
 import os
 import sys
+import nltk
+import csv
 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from apiclient.discovery import build_from_document
 from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
-
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the {{ Google Cloud Console }} at
-# {{ https://cloud.google.com/console }}.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = "client_secrets.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
 YOUTUBE_READ_WRITE_SSL_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-# This variable defines a message to display if the CLIENT_SECRETS_FILE is
-# missing.
 MISSING_CLIENT_SECRETS_MESSAGE = """
 WARNING: Please configure OAuth 2.0
 
@@ -49,43 +36,25 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
 
-# Authorize the request and store authorization credentials.
 def get_authenticated_service(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
+        message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage, args)
 
-  # Trusted testers can download this discovery document from the developers page
-  # and it should be in the same directory with the code.
-  with open("youtube-v3-discoverydocument.json", "r") as f:
-    doc = f.read()
-    return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
+    with open("youtube-v3-discoverydocument.json", "r") as f:
+        doc = f.read()
+        return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
 
-
-# Call the API's commentThreads.list method to list the existing comment threads.
-def get_comment_threads(youtube, video_id):
-  comments = []
-  results = youtube.commentThreads().list(
-    part="snippet",
-    videoId=video_id,
-    textFormat="plainText"
-  ).execute()
-
-  for item in results["items"]:
-    comment = item["snippet"]["topLevelComment"]
-    text = comment["snippet"]["textDisplay"]
-    comments.append(text)
-
-  while ("nextPageToken" in results):
+def get_comment_threads(youtube, video_id, comments=[], token=""):
     results = youtube.commentThreads().list(
         part="snippet",
+        pageToken=token,
         videoId=video_id,
-        pageToken=results["nextPageToken"],
         textFormat="plainText"
     ).execute()
 
@@ -93,7 +62,11 @@ def get_comment_threads(youtube, video_id):
         comment = item["snippet"]["topLevelComment"]
         text = comment["snippet"]["textDisplay"]
         comments.append(text)
-  return comments
+
+    if "nextPageToken" in results:
+        return get_comment_threads(youtube, video_id, comments, results["nextPageToken"])
+    else:
+        return comments
 
 if __name__ == "__main__":
   argparser.add_argument("--videoid",
@@ -104,13 +77,14 @@ if __name__ == "__main__":
     exit("Please specify videoid using the --videoid= parameter.")
 
   youtube = get_authenticated_service(args)
-  # All the available methods are used in sequence just for the sake of an example.
   try:
     video_comment_threads = get_comment_threads(youtube, args.videoid)
-    print(video_comment_threads)
-    print(len(video_comment_threads))
+    sia = SentimentIntensityAnalyzer()
+    with open('compounds.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for comment in video_comment_threads:
+            score = sia.polarity_scores(comment)
+            writer.writerow([comment, score["compound"]])
+    print("Logged sentiments of {0} comments to compounds.csv".format(len(video_comment_threads)))
   except HttpError as e:
     print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-  else:
-    print("Inserted, listed, updated, moderated, marked and deleted comments.")
-
